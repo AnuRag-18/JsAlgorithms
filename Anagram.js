da2f4de931b2b2611abb82c31f0059919ab0f956
AnuRag-18 <madhavarapurao720@gmail.com>
2019-01-21 17:31:05 +0530

    ANAGRAM

---
+++ Anagram.js
@@ -0,0 +1,24 @@
+function Anagram(first,second){
+    if(first.length !== second.length){
+        return false;
+    }
+    var letter = {};
+    for(var i = 0;i< first.length;i++){
+        // letter[first[i]]  = (letter[first[i]] || 0)+1;
+        letter[first[i]] ? letter[first[i]] +=1:letter[first[i]] =1;
+
+    }
+    for(var j = 0 ;j<second.length;j++){
+        if(!letter[second[j]]){
+            return false;
+        }
+        else {
+            letter[second[j]]-=1;
+        }
+    }
+    return true;
+    // console.log(letter);
+
+}
+console.log(Anagram("hello","elloh"));
+
