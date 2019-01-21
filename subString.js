adfa1cfeb6028ed58fc876a3bf44d1ca76b3ba09
AnuRag-18 <madhavarapurao720@gmail.com>
2019-01-22 04:09:16 +0530

    no of substrings in astring

---
+++ subString.js
@@ -0,0 +1,14 @@
+function subString(long,short){
+    var m=0;
+    for(var i=0;i<long.length;i++){
+        for(var j=0;j<short.length;j++){
+            if(short[j]!==long[i+j]) break;
+            
+            if(j===short.length-1){
+                m++;
+            }
+        }
+    }
+    return m;
+}
+console.log(subString("anuragrao sidddhu rao","rao"));
